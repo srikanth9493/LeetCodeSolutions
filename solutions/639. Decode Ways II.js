5be81f7d81bbcc0b4f7db9cc12f8f0887a697d1e
srikanth9493 <srikanth9949839402@gmail.com>
2021-07-15 22:42:30 +0530

    156 ms

--- solutions/639. Decode Ways II.js
+++ solutions/639. Decode Ways II.js
@@ -1,7 +1,40 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var numDecodings = function(s) {
+        
+        if(prev=='*'){
+            if(curr=='*'){
+                   dp[i]=(dp[i]+(15*dp[i-2]) %MOD) %MOD
+            }
+            else if(curr<='6'){
+                dp[i]=(dp[i]+(2*dp[i-2])%MOD)%MOD
+                       
+            }
+            else{
+                dp[i]=(dp[i]+dp[i-2])%MOD
+            }
+            
+        }
+        else if (prev=='1'){
+            if(curr=='*'){
+                dp[i]=(dp[i]+(9*dp[i-2])%MOD)%MOD
+            }
+            else{
+                dp[i]=(dp[i]+dp[i-2])%MOD
+            }
+        }
+        else if(prev=='2'){
+            if(curr=='*')
+                dp[i]=(dp[i]+(6*dp[i-2])%MOD)%MOD
+            else if (curr<='6'){
+                dp[i]=(dp[i]+dp[i-2])%MOD
+            }
+        }
+        
+        
+        
+        
+    }
+    console.log(dp)
+    return dp[dp.length-1]
+​
     
-};
+​
+}
