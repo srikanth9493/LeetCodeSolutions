8b7d566e823c60598a7201aeffc3eb4023549276
srikanth9493 <srikanth9949839402@gmail.com>
2021-10-10 15:25:17 +0530

    112 ms

---
+++ solutions/1143. Longest Common Subsequence.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+    
+    
+    let dp=new Array(text1.length+1).fill(0).map(()=>new Array(text2.length+1).fill(0))
+    
+    for(let i=0;i<=text1.length;i+=1){
+        for(let j=0;j<=text2.length;j+=1){
+            if(i==0 ||j==0){
+                dp[i][j]=0
+            }
+            else if(text1[i-1]==text2[j-1]){
+                dp[i][j]=1+dp[i-1][j-1]
+            }
+            else{
+                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1])
+            }
+        }
+    }
+  
+    
+   return dp[text1.length][text2.length]
+};
